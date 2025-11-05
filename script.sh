19cac7a02b20118fc86c4837c306bfa2f2713293
Taylor Ulve <ulvet@mail.gvsu.edu>
2025-11-05 18:12:37 -0500

    testing every piece on of script before combining

---
+++ script.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+
+arr=(10 20 40)
+
+<< 'comment'
+for arg in "${@:2}"
+do
+	num=0
+	while [ $num -lt 3 ];
+	do
+		python3 "$arg.py" "${arr[num]}" $1 >> "$arg.output"
+		num=$((num+1))
+	
+	done
+done
+comment
+
+num=0
+while [ $num -lt 3 ];
+do
+       echo $2.py
+       echo ${arr[num]}
+       echo $1
+       echo $2.output
+	
+       python3 $2.py ${arr[num]} $1 >> a.output
+
+       num=$((num+1))
+        
+
+done
